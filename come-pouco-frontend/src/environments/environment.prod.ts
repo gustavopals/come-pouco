0f8de1dcd9ea4d29f17b1cd66cbb946247fbe43c
Gustavo Pals <gustavoandrepals@gmail.com>
2026-05-21 10:30:34 -0300

    fix: update API URL to use HTTPS for production environment

--- come-pouco-frontend/src/environments/environment.prod.ts
+++ come-pouco-frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import { sentry } from './sentry.generated';
 
 export const environment = {
-  apiUrl: 'http://apicomepouco.palsincomehub.com/api',
+  apiUrl: 'https://apicomepouco.palsincomehub.com/api',
   appEnv: 'production',
   sentry: {
     dsn: sentry.dsn,
